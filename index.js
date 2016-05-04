1b187cd45df2e5806cf16407c4d3807722a53971
Serghei Ignat <seriojaignat@gmail.com>
2016-05-04 18:40:21 +0300

    Exposed the PurchaseLinks CRUD operations

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
 'use strict';
 
+module.exports = function(accessKey, /*optional*/hostName) {
+  //store the access key
+  GLOBAL.accessKey = accessKey
+
+  //const pjson = require('./package.json');
+  GLOBAL.standard_options = {
+      hostname: (typeof hostName === 'undefined')?'account.paydemic.com':hostName,
+      port: 443,
+      headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'User-Agent': 'paydemic-node/' + require('./package.json').version
+      }
+    };
+
+  //present all public methods
+  return {
+    PurchaseLinks: require('./lib/PurchaseLinks.js')
+  }
+}
+
+
+
