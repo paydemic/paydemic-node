ddce46a24cc35837e5428bfa8c3f730b71659e9c
Serghei Ignat <seriojaignat@gmail.com>
2016-05-10 17:35:42 +0300

    added CRUD.js - the Purchase Link test

--- test/CRUD.js
+++ test/CRUD.js
@@ -1,3 +1,16 @@
 "use strict";
-var assert = require("assert");
-var accessKey = require("./credentials/accessKey.js")
+
+var chai = require('chai');
+var expect = chai.expect;
+
+var chaiAsPromised = require('chai-as-promised');
+expect(chaiAsPromised).to.not.be.undefined;
+
+const accessKey = require("./credentials/accessKey.json")
+const domainHolder = require("./credentials/domain.json")
+
+const paydemic = require('../index')(accessKey, domainHolder.domain);
+
+expect(paydemic).to.not.be.undefined;
+expect(paydemic.PurchaseLinks).to.not.be.undefined;
+
