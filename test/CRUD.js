18dd04fbb2ae0d31eab12432dd064afb599ef37c
Serghei Ignat <seriojaignat@gmail.com>
2016-05-11 10:25:01 +0300

    added a test for PurchaseLinks.list()

--- test/CRUD.js
+++ test/CRUD.js
@@ -4,6 +4,8 @@ var chai = require('chai');
 var expect = chai.expect;
 
 var chaiAsPromised = require('chai-as-promised');
+chai.should();
+chai.use(chaiAsPromised)
 expect(chaiAsPromised).to.not.be.undefined;
 
 const accessKey = require("./credentials/accessKey.json")
@@ -14,3 +16,9 @@ const paydemic = require('../index')(accessKey, domainHolder.domain);
 expect(paydemic).to.not.be.undefined;
 expect(paydemic.PurchaseLinks).to.not.be.undefined;
 
+describe('List Purchase Links', () => {
+  it('should return an array', () => {
+    return paydemic.PurchaseLinks.list()
+      .should.eventually.be.an('array');
+  })
+})
